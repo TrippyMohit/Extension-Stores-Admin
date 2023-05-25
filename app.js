f313a18fe01e6fa64a8ecdc1eb179c3c17460dc6
trippyMohit <mohitnegi.trippybug@gmail.com>
2023-05-25 15:25:00 +0530

    vercel config added

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ import Routes from "./routes/route.js";
 import Connection from "./database/db.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/", Routes);
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 Connection();
 
